c1780cb2afc36de5e16aa9954346893d9b2372f1
muhammadeducation06-png <muhammad.education06@gmail.com>
2026-03-14 10:14:14 +0500

    updated

--- fifth.dart
+++ fifth.dart
@@ -22,20 +22,190 @@
 // }
 
 
-class Parent {
-  void show() {
-    print("Parent method");
-  }
-}
+// Single Level Inhertiance:-
 
-class Child extends Parent {
-  void show() {
-    print("Child method");
-    super.show();   // calling parent method
-  }
-}
+// class Parent {
+//   void show() {
+//     print("Parent method");
+//   }
+// }
 
-void main() {
-  Child c = Child();
-  c.show();
+// class Child extends Parent {
+//   void show() {
+//     print("Child method");
+//     super.show();   // calling parent method
+//   }
+// }
+
+// void main() {
+//   Child c = Child();
+//   c.show();
+// }
+
+// Hirerichal  Inhertiance:-
+
+// class Employee {
+//   String name;
+//   int id;
+
+//   Employee(this.name, this.id);
+
+//   void displayInfo() {
+//     print('Hey! ${name} and your id is${id}');
+//   }
+// }
+
+// class Developer extends Employee {
+
+//   Developer(super.name,super.id);
+
+//   void devInfo(){
+//     print('Hey! ${name} Dev your id ${id}');
+//   }
+// }
+
+
+// class Manager extends Employee {
+
+//   Manager(super.name,super.id);
+
+//   void managerInfo(){
+//     print('Hey ${name} Manger you id is${id}');
+//   }
+
+// }
+
+
+
+// Multi Level Inhertiance   Dart Not allowed:-
+
+
+
+
+// class Employee {
+//   String name;
+//   int id;
+
+//   Employee(this.name, this.id);
+
+//   void displayInfo() {
+//     print('Hey! ${name} and your id is${id}');
+//   }
+// }
+
+// class Developer extends Employee {
+
+//   Developer(super.name,super.id);
+
+//   void devInfo(){
+//     print('Hey! ${name} Dev your id ${id}');
+//   }
+
+// }
+
+
+// class Manager extends Developer {
+
+//   Manager(super.name,super.id);
+
+//   void managerInfo(){
+//     print('Hey ${name} Manger you id is${id}');
+//   }
+
+// }
+
+
+// Multiple Inhertiance:-
+
+
+// class Employee {
+//   String name;
+//   int id;
+
+//   Employee(this.name, this.id);
+
+//   void displayInfo() {
+//     print('Hey! ${name} and your id is${id}');
+//   }
+// }
+
+
+
+// class Manager  {
+
+//   void managerInfo(){
+//     print('Hey Manger ');
+//   }
+
+// }
+
+// class Developer extends Employee,Manager {
+
+//   Developer(super.name,super.id);
+
+//   void devInfo(){
+//     print('Hey! ${name} Dev your id ${id}');
+//   }
+  
+// }
+
+
+
+// Hybird Inhertiance   Dart Not allowed
+
+
+// class Parent {
+//   void info(){
+//     print('Hey Parent how are you/.');
+//   }
+// }
+
+// class Employee extends Parent{
+//   String name;
+//   int id;
+
+//   Employee(this.name, this.id);
+
+//   void displayInfo() {
+//     print('Hey! ${name} and your id is${id}');
+//   }
+// }
+
+
+
+// class Manager extends Parent {
+
+//   void managerInfo(){
+//     print('Hey Manger ');
+//   }
+
+// }
+
+// class Developer extends Employee,Manager {
+
+//   Developer(super.name,super.id);
+
+//   void devInfo(){
+//     print('Hey! ${name} Dev your id ${id}');
+//   }
+  
+// }
+
+
+
+
+
+
+
+
+
+void main(){
+
+  Manager manage = Manager('Huzaifa', 4321);
+
+  manage.displayInfo();
+  manage.devInfo();
+  manage.managerInfo();
+
+
 }
